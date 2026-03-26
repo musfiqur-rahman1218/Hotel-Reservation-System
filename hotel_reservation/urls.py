36e720540d2961acc61e76b4004d3a2a88ea1f6c
Md Musfiqur Rahman <md.musfiqur.rahman@smu.ca>
2026-03-25 21:19:34 -0300

    fix: redirect root URL to swagger UI

--- hotel_reservation/urls.py
+++ hotel_reservation/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/schema/swagger-ui/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('reservations.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
